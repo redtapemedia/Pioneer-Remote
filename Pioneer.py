af61d9a2d4cee4dc02ac64ba894fa830337901c2
Red Tape Media <info@redtapemedia.com>
2011-03-16 20:04:41 +1030

    First CLI Version... No XBMC integration as yet.

--- Pioneer.py
+++ Pioneer.py
@@ -1,8 +1,31 @@
+import sys
 import telnetlib
 
 HOST = "192.168.1.30"
+PORT = "8102"
 
-tn = telnetlib.Telnet(HOST,8102)
-tn.write("VU\r\n")
+class Main:
 
-#tn.close()
+    def _parse_argv( self ):
+        try:
+            self.PARAMS = sys.argv[1]
+            print "some params"
+        except:
+            sys.exit("no params")
+
+    def _send_command( self ):
+        try:
+            tn = telnetlib.Telnet(HOST,PORT)
+            tn.write(self.PARAMS + "\r\n")
+            tn.read_some()
+            tn.close()
+            print self.PARAMS
+        except:
+            print "Command Failed"
+
+    def __init__( self ):
+        self._parse_argv()
+        self._send_command()
+
+if ( __name__ == "__main__" ):
+    Main()
